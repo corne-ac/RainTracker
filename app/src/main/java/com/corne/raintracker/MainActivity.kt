76dad934a9c6d88f6acd1f058a1f875dd240e5d4
corne-ac <corne.ackerman1104@gmail.com>
2023-03-12 20:12:57 +0200

    git commit

--- app/src/main/java/com/corne/raintracker/MainActivity.kt
+++ app/src/main/java/com/corne/raintracker/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.corne.raintracker
 
+import com.corne.raintracker.R
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.corne.raintracker.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.selectedItemId = R.id.home
+
+        //OnClick events for bottom nav
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.add -> replaceFragment(AddFragment())
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.chart -> replaceFragment(ChartFragment())
+                R.id.list -> replaceFragment(ListFragment())
+            else -> {}
+            }
+            true
+        }
+
     }
+
+    //Function to replace frame_layout with the fragment
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
